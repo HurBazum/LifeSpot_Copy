317f407f2b87f3a8649d4091a3f184fe89513bbd
Anton Spiridonov <kilgoretrout633@gmail.com>
2023-07-11 00:51:26 +0700

    add tracker move functions

--- LifeSpot/Static/JS/Slide.js
+++ LifeSpot/Static/JS/Slide.js
@@ -5,28 +5,31 @@ const nameOfPictures = [
     "https://localhost:5001/Static/SlidePictures/ny.jpg",
     "https://localhost:5001/Static/SlidePictures/tokyoCut.jpg"
 ];
-
+let startW;
+let startH;
 // тест
-// выравнивает div с кружками примерно по центру slider'а
-function foo() {
+// выравнивает div с кружками примерно по центру slider'а,
+// задаёт startW - изначальная ширина слайдера, с которой
+// потом сравнивается его же ширина после события resize,
+// чтобы выполнилось изменение положения трекера moveTracker()
+// также запоминает начальныt ширину и высоту window
+function setTracker() {
     const sldr = document.getElementsByClassName('slider')[0];
     if (sldr == undefined) {
         console.log('error!');
     }
     else {
-        foo1(sldr);
-        let sliderWidth = +getComputedStyle(sldr).width.replace('px','');
+        foo(sldr);
+        startW = window.innerWidth;
+        startH = window.innerHeight;
+        let sliderWidth = +getComputedStyle(sldr).width.replace('px', '');
         let trackerWidth = +getComputedStyle(sldr.children[1]).width.replace('px', '');
-        console.log(sliderWidth);
-        console.log(trackerWidth);
-        let centerPosition = (trackerWidth / sliderWidth) * 50;
-        console.log(centerPosition)
-        sldr.children[1].style.left = +sldr.children[1].style.left.replace('%', '') - centerPosition + '%';
+        sldr.children[1].style.marginLeft = (sliderWidth / 2) - (trackerWidth / 2) + 'px';
     }
 }
 
 // вывод ссылки на текущую картинку в консоль
-function foo1(x) {
+function foo(x) {
     let g = new Array();
     for (let ch of Array.from(x.children)) {
         let v = ch.querySelector('img');
@@ -64,7 +67,7 @@ function changeBubble(bubbles, index, direction) {
 }
 
 // изменение текущей картинки, при помощи кнопок на слайдере
-function changePicture(id) {
+function browseImgByButtons(id) {
     let currenrPicture = document.getElementsByClassName('slider')[0].children[0].querySelector('img');
     let link = currenrPicture.src;
     let b = document.getElementsByClassName('bub');
@@ -109,20 +112,20 @@ function getSliderHeight() {
 // #d32d2d; - базовый цвет кружкка
 // #ae4c4c; - новый цвет
 // смена картинки в слайдере с помощью стрелок
-function pressArrowsToBrowseImg(event) {
+function browseImgByArrows(event) {
     let currenrPicture = document.getElementsByClassName('slider')[0].children[0].querySelector('img');
     let link = currenrPicture.src;
-    let b = document.getElementsByClassName('bub');
+    let bubbles = document.getElementsByClassName('bub');
 
     if (event.key === "ArrowRight") {
         if (link != nameOfPictures[nameOfPictures.length - 1]) {
             let newSrc = nameOfPictures.indexOf(link) + 1;
             link = nameOfPictures[newSrc];
-            changeBubble(b, newSrc, true);
+            changeBubble(bubbles, newSrc, true);
         }
         else {
             link = nameOfPictures[0];
-            changeBubble(b, 0, true);
+            changeBubble(bubbles, 0, true);
         }
         currenrPicture.src = link;
     }
@@ -130,14 +133,26 @@ function pressArrowsToBrowseImg(event) {
         if (link != nameOfPictures[0]) {
             let newSrc = nameOfPictures.indexOf(link) - 1;
             link = nameOfPictures[newSrc];
-            changeBubble(b, newSrc, false);
+            changeBubble(bubbles, newSrc, false);
         }
         else {
             link = nameOfPictures[nameOfPictures.length - 1];
-            changeBubble(b, nameOfPictures.length - 1, false);
+            changeBubble(bubbles, nameOfPictures.length - 1, false);
         }
         currenrPicture.src = link;
     }
+}
+
+function resizeWindow() {
+    setInterval(() => {
+        if (window.innerHeight != startH || window.innerWidth != startW) {
+            const sldr = document.getElementsByClassName('slider')[0];
+            document.getElementsByClassName('tracker')[0].style.marginLeft =
+                +getComputedStyle(sldr).width.replace('px', '') / 2 - +getComputedStyle(sldr.children[1]).width.replace('px', '') / 2 + 'px';
+            startW = window.innerWidth;
+            startH = window.innerHeight;
+        }
+    }, 20);
 }
 
 // 
@@ -147,29 +162,29 @@ window.onload = function () {
 
     // скрыть кнопки на слайдере
     sldr.addEventListener("mouseout", function () {
-        let x = document.getElementsByClassName('navigator');
-        let o = sldr.getElementsByClassName('tracker')[0];
-        for (let y of x) {
-            y.style.display = '';
+        let nav = document.getElementsByClassName('navigator');
+        let track = sldr.getElementsByClassName('tracker')[0];
+        for (let elem of nav) {
+            elem.style.display = '';
         }
-        o.style.bottom = '';
+        track.style.bottom = '';
         sldr.style.margin = '';
     });
 
     // показать кнопки на слайдере
     sldr.addEventListener("mouseover", function () {
-        let x = document.getElementsByClassName('navigator');
-        let o = sldr.getElementsByClassName('tracker')[0];
-        for (let y of x) {
-            y.style.display = 'inline-flex';
+        let nav = document.getElementsByClassName('navigator');
+        let track = sldr.getElementsByClassName('tracker')[0];
+        for (let elem of nav) {
+            elem.style.display = 'inline-flex';
         }
-        o.style.bottom = '3vh';
+        track.style.bottom = '3vh';
         sldr.style.margin = '0 0 -7vh 0';
     });
 }
 
 // задаёт цвет для первого кружка, при загрузке страницы
-function startBubble() {
+function drawStartBubble() {
     let currPic = document.querySelector('img').src;
     let indexCurrPic = nameOfPictures.indexOf(currPic);
     document.getElementsByClassName('bub')[indexCurrPic].style.backgroundColor = "#ae4c4c";
@@ -177,28 +192,44 @@ function startBubble() {
 
 // создаЄт кружки под картинкой, кот. показывают общее кол-во картинок 
 // и текущую картинку, чуть измен¤¤ свой цвет
-function addBub(amount, parent) {
+function addBubbles(amount, parent) {
     for (let i = amount - 1; i > -1; i--) {
         parent.insertAdjacentHTML("afterbegin", '<div class="bub"><div>');
     }
 }
 
 // создаЄт коллекцию кружков под слайдером сразу после загрузки
-document.addEventListener('DOMContentLoaded', addBub(nameOfPictures.length, document.getElementsByClassName('tracker')[0]));
+document.addEventListener('DOMContentLoaded', addBubbles(nameOfPictures.length, document.getElementsByClassName('tracker')[0]));
 
 // задаЄт цвет первого кружка под слайдером сразу после загрузки
-document.addEventListener('DOMContentLoaded', startBubble);
+document.addEventListener('DOMContentLoaded', drawStartBubble);
 
 // тест
-document.addEventListener('DOMContentLoaded', foo);
+document.addEventListener('DOMContentLoaded', setTracker);
+
+// проверяет текущий размер окна и двигает блок tracker, если нужно, 
+// т.к.событие onresize срабатывает только, когда окно растягивается вручную
+document.addEventListener('DOMContentLoaded', resizeWindow);
 
 // картинки листаются с помощью стрелок, если предварительно кликнуть по картинке,
 // если кликнуть в другом месте - листать с помощью стрелок уже будет нельзя!
 window.onclick = function (event) {
     if (event.target == document.querySelector('img')) {
-        window.addEventListener("keydown", pressArrowsToBrowseImg);
+        window.addEventListener("keydown", browseImgByArrows);
     }
     else {
-        window.removeEventListener("keydown", pressArrowsToBrowseImg);
+        window.removeEventListener("keydown", browseImgByArrows);
+    }
+}
+
+// изменяет положение трекера, при событии resize
+window.onresize = function (event) {
+    const sldr = document.getElementsByClassName('slider')[0];
+    if (startW != +getComputedStyle(sldr).width.replace('px', '')) {
+        let timer = setTimeout(() => {
+            document.getElementsByClassName('tracker')[0].style.marginLeft =
+                +getComputedStyle(sldr).width.replace('px', '') / 2 - +getComputedStyle(sldr.children[1]).width.replace('px', '') / 2 + 'px';
+        }, 20);
+        startW = sldr.style.width;
     }
 }
